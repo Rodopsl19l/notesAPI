380f5c369f65d374dfafb17f1bb6fd0c2720f604
rodops <rodops19@gmail.com>
2021-01-20 01:31:02 -0600

    Primer commit

---
+++ Notes.Core/INotesServices.cs
@@ -0,0 +1,16 @@
+﻿using Notes.DB;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Core
+{
+    public interface INotesServices
+    {
+        List<Note> GetNotes();
+        Note GetNoteById(int id);
+        Note CreateNote(Note note);
+        void DeleteNote(int id);
+        void EditNote(Note note);
+    }
+}
---
+++ Notes.DB/Note.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Notes.DB
+{
+    public class Note
+    {
+        [Key]
+        public int Id { get; set; }
+        public string value { get; set; }
+    }
+}
